30a22e5d1549da3d9ea43e0961177e5a0e49159e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-10-30 17:33:44 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -63,7 +63,7 @@ const HelloWorldIntentHandler = {
         'Subscription-Key': "asd"
       }
     };
-    await https.post('https://sch.barmetler.com/alexa/storeuser', handlerInput)
+    // await https.post('https://sch.barmetler.com/alexa/storeuser', handlerInput)
     return https
         .get('https://sch.barmetler.com/alexa/help', get_options)
         .then(responseString => {
